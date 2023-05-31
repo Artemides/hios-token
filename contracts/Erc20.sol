f6916bd390ac4d3a9aaac04a95665a0824adb74c
Artemides <edmpulasky@gmail.com>
2023-05-31 18:08:08 -0500

    create erc20 with open zeppelin

---
+++ contracts/Erc20.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract HiosZeppelin is ERC20 {
+    constructor(
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        uint256 _initialSupply
+    ) ERC20(_tokenName, _tokenSymbol) {
+        _mint(msg.sender, _initialSupply);
+    }
+}
