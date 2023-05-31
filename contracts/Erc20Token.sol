208b4a13d8406d824163388c305ec598d8a80619
Artemides <edmpulasky@gmail.com>
2023-05-31 13:32:25 -0500

    implement transferFrom third pary

--- contracts/Erc20Token.sol
+++ contracts/Erc20Token.sol
@@ -44,6 +44,17 @@ contract ERC20 {
         return true;
     }
 
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
     function approve(address _spender, uint256 _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
